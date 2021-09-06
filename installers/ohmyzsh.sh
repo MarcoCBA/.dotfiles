7d5ac03ebeb1ea7a53940539c95009602529c900
MarcoCBA <markitocab@gmail.com>
2021-09-06 14:38:28 -0300

    Fix ohmyszh installation

--- installers/ohmyzsh.sh
+++ installers/ohmyzsh.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Install Zsh & Oh My Zsh
-yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
