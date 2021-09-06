dccc06df0dbdabebf9f71d7af37c90113fd6f499
MarcoCBA <markitocab@gmail.com>
2021-09-06 14:51:23 -0300

    Fix auto run zsh after installation

--- installers/ohmyzsh.sh
+++ installers/ohmyzsh.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Do not run zsh after installation
+RUNZSH=no
+
 # Install Zsh & Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
