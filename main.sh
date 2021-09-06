1ece416a3064e1d0eeaa47ceeccfbe4872e51897
MarcoCBA <markitocab@gmail.com>
2021-09-06 14:11:44 -0300

    Fix logging

--- main.sh
+++ main.sh
@@ -8,21 +8,21 @@ RCS_DIR=$HOME/.dotfiles/rc
 CONFIGS_DIR=$HOME/.dotfiles/configs
 
 # Run installers
-log_info "Installing xcode..." > /dev/null
+log_info "Installing xcode..."
 $INSTALLERS_DIR/xcode.sh > /dev/null
-log_success "Success!"
+log_success "Success"
 
 log_info "Installing Oh My ZSH..."
 $INSTALLERS_DIR/ohmyzsh.sh > /dev/null
-log_success "Success!"
+log_success "Success"
 
-log_info "Installing Powerlevel10k..." > /dev/null
+log_info "Installing Powerlevel10k..."
 $INSTALLERS_DIR/p10k.sh > /dev/null
-log_success "Success!"
+log_success "Success"
 
 log_info "Installing homebrew, its configs and requierements..."
 $INSTALLERS_DIR/brew.sh > /dev/null
-log_success "Success!"
+log_success "Success"
 
 # Install rc dotfiles
 safe_ln ~/.dotfiles/git-template/gitconfig.global ~/.gitconfig
