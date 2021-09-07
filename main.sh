fb15845de95809b768c462fe8c6c68a724fa2dfd
MarcoCBA <markitocab@gmail.com>
2021-09-07 16:48:55 -0300

    Add selector in main script

--- main.sh
+++ main.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
+# Dotfiles Install Script
+# {{{ Parameters:
+# - none: install everything
+# - INSTALL-ALL: run all installers
+# - XCODE: install xcode clt
+# - OH-MY-ZSH: install Oh-My-Zsh scripts
+# - P10K: install Powerlevel10k theme for ohmyzsh
+# - BREW: install homebrew, update, and install some requierements
+# - RCS: install all dotfiles
+# - GITRC: install git dotfiles
+# - ZSHRC: install zsh dotfiles
+# - P10KRC: install Powerlevel10k already configured theme
+# - NVIMRC: install nvim dotfiles.
+# - CONFIGURE: run all configuration scripts
+# - DOCK: configure dock
+# - FINDER: configure Finder
+# - INPUT: configure text input
+# - LANG: configure language
+# - TIME-MACHINE: configure time machine
+# - MANUAL: list manual configuration tasks
+# }}}
+
 
 # Load utilities
 source ~/.dotfiles/utils/utils.sh
@@ -8,55 +30,80 @@ RCS_DIR=$HOME/.dotfiles/runcom
 CONFIGS_DIR=$HOME/.dotfiles/config
 ASSETS_FOLDER=$HOME/.dotfiles/assets
 
+# Add spaces to enable looking for isolated commands
+args=" $* "
+
 # Run installers
-log_info "Installing xcode..."
-$INSTALLERS_DIR/xcode.sh
-log_success "Installation successful"
+if [[ $args == *\ XCODE\ * ]] || [[ $args == *\ INSTALL-ALL\ * ]] || [[ $# -eq 0 ]]; then
+    log_info "Installing xcode..."
+    $INSTALLERS_DIR/xcode.sh
+    log_success "Installation successful"
+fi
 
-log_info "Installing Oh My ZSH..."
-$INSTALLERS_DIR/ohmyzsh.sh
-log_success "Installation successful"
+if [[ $args == *\ OH-MY-ZSH\ * ]] || [[ $args == *\ INSTALL-ALL\ * ]] || [[ $# -eq 0 ]]; then
+    log_info "Installing Oh My ZSH..."
+    $INSTALLERS_DIR/ohmyzsh.sh
+    log_success "Installation successful"
+fi
 
-log_info "Installing Powerlevel10k..."
-$INSTALLERS_DIR/p10k.sh
-log_success "Installation successful"
+if [[ $args == *\ P10K\ * ]] || [[ $args == *\ INSTALL-ALL\ * ]] || [[ $# -eq 0 ]]; then
+    log_info "Installing Powerlevel10k..."
+    $INSTALLERS_DIR/p10k.sh
+    log_success "Installation successful"
+fi
 
-log_info "Installing homebrew, its configs and requierements..."
-$INSTALLERS_DIR/brew.sh
-log_success "Installation successful"
+if [[ $args == *\ BREW\ * ]] || [[ $args == *\ INSTALL-ALL\ * ]] || [[ $# -eq 0 ]]; then
+    log_info "Installing homebrew, its configs and requierements..."
+    $INSTALLERS_DIR/brew.sh
+    log_success "Installation successful"
+fi
 
 # Install rc dotfiles
-safe_ln $RCS_DIR/.gitrc ~/.gitconfig
-log_success "GIT: Setup success"
+if [[ $args == *\ GITRC\ * ]] || [[ $args == *\ RCS\ * ]] || [[ $# -eq 0 ]]; then
+    safe_ln $RCS_DIR/.gitrc ~/.gitconfig
+    log_success "GIT: Setup success"
+fi
 
-safe_ln $RCS_DIR/.zshrc ~/.zshrc
-log_success "ZSH: Setup success"
+if [[ $args == *\ ZSHRC\ * ]] || [[ $args == *\ RCS\ * ]] || [[ $# -eq 0 ]]; then
+    safe_ln $RCS_DIR/.zshrc ~/.zshrc
+    log_success "ZSH: Setup success"
+fi
 
-safe_ln $RCS_DIR/.p10krc ~/.p10krc
-log_success "P10K: Setup success"
+if [[ $args == *\ P10KRC\ * ]] || [[ $args == *\ RCS\ * ]] || [[ $# -eq 0 ]]; then
+    safe_ln $RCS_DIR/.p10krc ~/.p10krc
+    log_success "P10K: Setup success"
+fi
 
-NVIM_CONF_PATH=$HOME/.config/nvim
-mkdir -p $NVIM_CONF_PATH # Path may not exist
-safe_ln $RCS_DIR/.nvimrc $NVIM_CONF_PATH/init.vim
-log_success "NVIM: Setup success"
+if [[ $args == *\ NVIMRC\ * ]] || [[ $args == *\ RCS\ * ]] || [[ $# -eq 0 ]]; then
+    NVIM_CONF_PATH=$HOME/.config/nvim
+    mkdir -p $NVIM_CONF_PATH # Path may not exist
+    safe_ln $RCS_DIR/.nvimrc $NVIM_CONF_PATH/init.vim
+    log_success "NVIM: Setup success"
+fi
 
 # Run configuration scripts
-log_info "Setting up macOS..."
+if [[ $args == *\ DOCK\ * ]] || [[ $args == *\ CONFIGURE\ * ]] || [[ $# -eq 0 ]]; then
+    $CONFIGS_DIR/macOS/dock.sh
+    log_success "DOCK: Configured"
+fi
 
-$CONFIGS_DIR/macOS/dock.sh
-log_success "DOCK: Configured"
+if [[ $args == *\ FINDER\ * ]] || [[ $args == *\ CONFIGURE\ * ]] || [[ $# -eq 0 ]]; then
+    $CONFIGS_DIR/macOS/finder.sh
+    log_success "FINDER: Configured"
+fi
 
-$CONFIGS_DIR/macOS/finder.sh
-log_success "FINDER: Configured"
+if [[ $args == *\ INPUT\ * ]] || [[ $args == *\ CONFIGURE\ * ]] || [[ $# -eq 0 ]]; then
+    $CONFIGS_DIR/macOS/input.sh
+    log_success "INPUT: Configured"
+fi
 
-$CONFIGS_DIR/macOS/input.sh
-log_success "INPUT: Configured"
+if [[ $args == *\ LANG\ * ]] || [[ $args == *\ CONFIGURE\ * ]] || [[ $# -eq 0 ]]; then
+    $CONFIGS_DIR/macOS/lang.sh
+    log_success "LANGUAGE: Configured"
+fi
 
-$CONFIGS_DIR/macOS/lang.sh
-log_success "LANGUAGE: Configured"
-
-$CONFIGS_DIR/macOS/timemachine.sh
-log_success "TIME MACHINE: Configured"
-
-
+if [[ $args == *\ TIME-MACHINE\ * ]] || [[ $args == *\ CONFIGURE\ * ]] || [[ $# -eq 0 ]]; then
+    $CONFIGS_DIR/macOS/timemachine.sh
+    log_success "TIME MACHINE: Configured"
+fi
 
