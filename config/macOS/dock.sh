dd5c308542a6dbd30072fd6857014c6471c1cf3a
MarcoCBA <markitocab@gmail.com>
2021-09-07 14:00:26 -0300

    Add dock ordering

--- config/macOS/dock.sh
+++ config/macOS/dock.sh
@@ -7,5 +7,13 @@ defaults write com.apple.dock autohide-delay -float 0
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
 
+# Reorder dock
+dockutil --remove all
+REQUIREMENTS=$HOME/.dotfiles/requirements/dock/apps.txt
+APPLICATION_PATH=/Applications
+while read p; do
+    dockutil --add $APPLICATION_PATH/$p
+done < REQUIREMENTS
+
 killall Dock
 
