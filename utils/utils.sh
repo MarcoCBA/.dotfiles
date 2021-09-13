7c854d2aabc507a91f8146c82642bcf9dfe308ab
MarcoCBA <markitocab@gmail.com>
2021-09-12 22:05:32 -0300

    Add manual action utility

--- utils/utils.sh
+++ utils/utils.sh
@@ -11,6 +11,12 @@ cyan='\033[0;36m'
 white='\033[0;37m'
 NC='\033[0m'
 
+# Manual actions
+function log_manual() {
+    echo -e "${red}>>> MANUAL: $@ <<<${NC}"
+    read -r -p "Press any key to continue..."
+}
+
 # Log utils
 function log_info() { echo -e "${cyan}>>> $@ <<<${NC}"; }
 function log_success() { echo -e "${green}>>> $@ <<<${NC}"; }
