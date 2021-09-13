039b66146e8832e59505afcf6320e1e71628fcf5
MarcoCBA <markitocab@gmail.com>
2021-09-12 22:06:10 -0300

    Add excecute permissions to scripts

--- config/macOS/iterm.sh
+++ config/macOS/iterm.sh
@@ -2,8 +2,7 @@
 
 # Install the Solarized Dark theme for iTerm
 open ~/.dotfiles/assets/iterm2/nord.itermcolors
-log_info "Set theme as default in iterm2 settings"
-read -r -p "Press any key to continue..." key
+log_manual "Set theme as default in iterm2 settings"
 
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
