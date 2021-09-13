3c5b9878fd68971c1927c5cd4100f8a8de3e15ab
MarcoCBA <markitocab@gmail.com>
2021-09-12 21:00:20 -0300

    Add theme installation for iterm

--- config/macOS/iterm.sh
+++ config/macOS/iterm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install the Solarized Dark theme for iTerm
-# TODO: insert a theme in assets as: open "${HOME}/init/Solarized Dark.itermcolors"
+open ~/.dotfiles/assets/iterm2/nord.itermcolors
 
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
