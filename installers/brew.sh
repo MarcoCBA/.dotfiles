ede4ec8c84ca2dc314e8a6ef92d6240f4bb6f3a5
MarcoCBA <markitocab@gmail.com>
2022-03-07 14:10:57 -0300

    Update brew instalation command.

--- installers/brew.sh
+++ installers/brew.sh
@@ -12,9 +12,3 @@ xargs brew install < ~/.dotfiles/requirements/brew/brews.txt
 # Install casks
 xargs brew install --cask --appdir="/Applications" < ~/.dotfiles/requirements/brew/casks.txt
 
-# Print extra steps and add wait for user to continue
-while read p; do
-    log_info "$p"
-done < ~/.dotfiles/requirements/brew/extra_steps.txt
-read -r -p "Press any key to continue..." key
-
