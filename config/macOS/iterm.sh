381197ab25eaa90489d664f3fcc8906815e8abd6
MarcoCBA <markitocab@gmail.com>
2022-03-19 18:58:22 -0300

    Enable mouse in item for vim.

--- config/macOS/iterm.sh
+++ config/macOS/iterm.sh
@@ -2,8 +2,11 @@
 
 # Install the Solarized Dark theme for iTerm
 open ~/.dotfiles/assets/iterm2/nord.itermcolors
-log_manual "Set theme as default in iterm2 settings"
+open ~/.dotfiles/assets/p10k/MesloLGS\ NF\ Regular.ttf
 
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+# Allow alternate mouse scrolling.
+defaults write com.googlecode.iterm2 AlternateMouseScroll -bool true
 
