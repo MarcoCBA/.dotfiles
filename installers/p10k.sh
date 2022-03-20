55242a697f2e5ef38ebbddda8c28777a55016ce6
MarcoCBA <markitocab@gmail.com>
2022-03-20 19:23:23 -0300

    Remove useless key prompt.

--- installers/p10k.sh
+++ installers/p10k.sh
@@ -3,5 +3,3 @@
 # Install Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-read -r -p "Press any key to continue..." key
-
